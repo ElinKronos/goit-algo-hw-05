d8e3b1ec57f72197039e8a42e382c32a0bb8c6d9
ElinKronos <elinder2010@gmail.com>
2025-07-16 20:44:47 +0300

    All tasks done

---
+++ 04_Fourth_Task_Chat_Bot_Mini+.py
@@ -0,0 +1,95 @@
+# Here begins a new world...
+
+# Ця програма створює невеличкий CLI застосунок для збереження імен користувачів та їхніх телефонних номерів
+
+from collections import UserDict
+
+class ChatBot(UserDict):
+    def __init__(self):
+        super().__init__()
+
+    def __str__(self):
+        return "\n".join(f"{name}: {phone}" for name, phone in self.data.items())
+
+def main():
+    contacts = ChatBot()
+    print("Welcome to the assistant bot!")
+    while True:
+        user_input = input("Enter a command: ")
+        command, args = parse_input(user_input)
+
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+        elif command == "hello":
+            print("How can I help you?")
+        elif command == "add":
+            print(add_contact(args, contacts))
+        elif command == "change":
+            print(change_contact(args, contacts))
+        elif command == "phone":
+            print(show_phone(args, contacts))
+        elif command == "all":
+            print(show_all(contacts))
+        else:
+            print("Invalid command.")
+
+# Функція створення декоратора
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except KeyError:
+            return "Enter user name."
+        except ValueError:
+            return "Give me name and phone please."
+        except IndexError:
+            return "Enter the argument for the command."
+    return inner
+
+# Парсер команд
+def parse_input(user_input):
+    cmd, *args = user_input.strip().split()
+    return cmd.lower(), args
+
+# Додавання контакту
+@input_error
+def add_contact(args, contacts):
+    if len(args) != 2:
+        return "Usage: add <name> <phone>"
+    name, phone = args
+    contacts[name] = phone
+    return "Contact added."
+
+# Зміна контакту
+@input_error
+def change_contact(args, contacts):
+    if len(args) != 2:
+        return "Usage: change <name> <new_phone>"
+    name, phone = args
+    if name in contacts:
+        contacts[name] = phone
+        return "Contact updated."
+    else:
+        return "Contact not found."
+
+# Виводить номер телефону вибраного користувача
+@input_error
+def show_phone(args, contacts):
+    if len(args) != 1:
+        return "Usage: phone <name>"
+    name = args[0]
+    if name in contacts:
+        return f"{name}: {contacts[name]}"
+    else:
+        return "Contact not found."
+
+# Виводить усі збережені контакти
+@input_error
+def show_all(contacts):
+    if not contacts:
+        return "No contacts found."
+    return "\n".join(f"{name}: {phone}" for name, phone in contacts.items())
+
+if __name__ == "__main__":
+    main()
